3000def8215d64f6fd9c34ab114de0891c749a59
mina3010 <menamaged833@gmail.com>
2023-08-10 16:33:52 +0300

    first commit

--- app/src/main/java/com/minamagid/mazaady/common/Constants.kt
+++ app/src/main/java/com/minamagid/mazaady/common/Constants.kt
@@ -2,6 +2,4 @@ package com.minamagid.mazaady.common
 
 object Constants {
     const val BASE_URL = "https://staging.mazaady.com/api/v1/"
-    const val API_TOKEN = "3%o8i}_;3D4bF]G5@22r2)Et1&mLJ4?\$@+16"
-
 }
